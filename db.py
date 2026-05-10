3fd2e41637a371c4b7aa126686a8d184a85672dd
Isaias <isaias@email.com>
2026-05-10 11:14:20 -0600

    Separacion de bases publica y privada

--- db.py
+++ db.py
@@ -2,13 +2,9 @@ import mysql.connector
 import os
 
 def get_connection():
-    try:
-        return mysql.connector.connect(
-            host="db",
-            user="root",
-            password="1234",
-            database="logistica"
-        )
-    except Exception as e:
-        print("ERROR DE CONEXIÓN:", e)
-        raise
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST", "db"),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", "1234"),
+        database="logistica_privada"
+    )
